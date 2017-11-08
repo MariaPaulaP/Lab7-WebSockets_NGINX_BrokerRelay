291831a3b903c3a31f6803de69a3bed6ec85cde0
MariaPaulaP <maria.pinzon-c@mail.escuelaing.edu.co>
2017-11-08 17:46:45 -0500

    inicio parte 2

---
+++ src/main/java/edu/eci/arsw/collabhangman/cache/GameStateRedisCache.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.collabhangman.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author 2095957
+ */
+public class GameStateRedisCache {
+
+    @Autowired
+    private String RedisTemplate template;
+}
---
+++ src/main/java/edu/eci/arsw/collabhangman/cache/HangmanRedisGame.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.collabhangman.cache;
+
+import edu.eci.arsw.collabhangman.model.game.HangmanGame;
+
+/**
+ *
+ * @author 2095957
+ */
+public class HangmanRedisGame extends HangmanGame{
+    
+    public HangmanRedisGame(String word) {
+        super(word);
+    }
+    
+    
+    
+   
+}
